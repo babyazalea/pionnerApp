91b34043ce6fe8eefdb0d0cf00956550fea9cfe1
babyazalea <usun12@hotmail.com>
2019-05-01 17:43:22 +0900

    190501 modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './img/pionner_logo.jpg';
 import Pionner from './Pionner';
 
-function App() {
-  return (
-    <div className="App">
-      <Pionner />
-    </div>
-  );
+
+// _callApi = () => {
+  //   fetch('https://newsapi.org/v2/top-headlines?country=kr&apiKey=30aba0364ea24b4ebce7366e8d20339d')
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
+  //   .catch(err => console.log(err))
+  // }
+  
+  class App extends Component {
+ 
+  state={}
+  
+  componentDidMount(){
+    this._getArticles();
+  }
+  
+  _renderMain= () => {
+    const articles = this.state.articles.map((article, index) => {
+      console.log(index);
+      if(index){
+        return <Pionner title={article.title} url={article.url} key={index} />
+      } else{
+      };
+    })
+    return articles
+  }
+  
+  _getArticles = async () => {
+    const articles = await this._callApi()
+    this.setState({
+      articles
+    })
+  }
+
+  _callApi = () =>{
+    return fetch('https://newsapi.org/v2/top-headlines?country=kr&apiKey=30aba0364ea24b4ebce7366e8d20339d')
+    .then(response => response.json())
+    .then(json => json.articles)
+    .catch(err => console.log(err))
+  }
+  
+  render(){
+    return (
+      <div className="App">
+            <div className="Pionner">
+            <header>
+                <div className="headerWrap">
+                <div className="headerLeft">
+                    <a href="#" className="title">
+                    <h1><img src={logo} alt="pionner logo"/></h1>
+                    </a>
+                </div>{/* headerLeft */}
+                <div className="headerRight">
+                    <div className="toggle">
+                    <form>
+                        <input type="range" className="test" onChange={(e)=> console.log(e.currentTarget.value)}></input>
+                    </form>
+                    </div>    
+                </div>{/* headerRight */}
+                </div>{/* headerWrap */}
+            </header>
+            <div className="mainVisual">
+                <ul>
+                {this.state.articles ? this._renderMain() : 'LOADING'}
+                </ul>
+            </div>
+        </div>
+        </div>
+    );
+  }
 }
+
 
 export default App;
