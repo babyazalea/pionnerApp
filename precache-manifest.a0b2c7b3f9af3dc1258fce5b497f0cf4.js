d88d356c12fd2e22e96e452ca44ca295a6af2234
babyazalea <usun12@hotmail.com>
2019-05-16 10:06:21 +0900

    Updates

---
+++ precache-manifest.a0b2c7b3f9af3dc1258fce5b497f0cf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7987058967855017b0075930de5c4713",
+    "url": "/pionnerApp/index.html"
+  },
+  {
+    "revision": "b60e9b43d724acaa7062",
+    "url": "/pionnerApp/static/css/main.1903c23a.chunk.css"
+  },
+  {
+    "revision": "baa71292b4cf032c934e",
+    "url": "/pionnerApp/static/js/2.50ddd0e6.chunk.js"
+  },
+  {
+    "revision": "b60e9b43d724acaa7062",
+    "url": "/pionnerApp/static/js/main.36e0312f.chunk.js"
+  },
+  {
+    "revision": "264c71e2601b8a7d8188",
+    "url": "/pionnerApp/static/js/runtime~main.b1dbe409.js"
+  },
+  {
+    "revision": "9581cc32ef3d9f8035bdbd48d852d127",
+    "url": "/pionnerApp/static/media/pionner_logo.9581cc32.jpg"
+  },
+  {
+    "revision": "2de98754b514d9a217cbba4d9420d5c1",
+    "url": "/pionnerApp/static/media/the_expanding_circle.2de98754.png"
+  }
+]);
